34ce153912df7e04460236eb87b1878ecab7fd21
iamalex <ngashalex8@gmail.com>
2023-05-08 18:45:25 +0300

    added es6 modules

---
+++ modules/navlist.js
@@ -0,0 +1,6 @@
+export const navList = document.querySelector('.nav-list');
+export const navAdd = document.querySelector('.nav-add');
+export const navContact = document.querySelector('.nav-contact');
+export const listSection = document.querySelector('.list');
+export const addANewSection = document.querySelector('.add-a-new-book');
+export const contactSection = document.querySelector('.contact');
